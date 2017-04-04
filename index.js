d9ad66419f25de1ef6dc935553aa3b68b79fb0b9
John <jlek@findmypast.com>
2017-04-04 14:21:45 +0100

    Implemented age description generation. Refactor required.

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-const {path, pipe, sortBy, toLower} = require('ramda');
+const {curry, flip, map, path, pipe, prop, sortBy, toLower} = require('ramda');
 
 const getSurname = path(['name', 'sur']);
 const getSurnameLower = pipe(getSurname, toLower);
 const sortBySurname = sortBy(getSurnameLower);
 
+const getId = prop('id');
+const calculateYearDifference = (dateOne, dateTwo) => {
+  let yearDifference = dateOne.getFullYear() - dateTwo.getFullYear();
+  const monthDifference = dateOne.getMonth() - dateTwo.getMonth();
+  if (
+    monthDifference < 0
+    || (monthDifference === 0 && dateOne.getDate() < dateTwo.getDate())
+  ) {
+    yearDifference--;
+  }
+  return yearDifference;
+};
+// const calculateAge = (date, dateOfBirth) => 38; // TODO
+const formatName = ({name}) => `${name.first} ${name.sur}`;
+const getAgeDescription = curry((date, datesOfBirth, person) => {
+  const dateOfBirth = pipe(
+    getId,
+    flip(prop)(datesOfBirth)
+  )(person);
+
+  return dateOfBirth
+    ? `${formatName(person)} is ${calculateYearDifference(date, dateOfBirth)} years old.`
+    : `${formatName(person)}'s age is unknown.`;
+});
+const getAgeDescriptions = curry((date, datesOfBirth, people) => {
+  return map(getAgeDescription(date, datesOfBirth), people);
+});
+
 module.exports = {
   sortBySurname,
+  getAgeDescriptions,
 };
--- index.spec.js
+++ index.spec.js
@@ -1,35 +1,59 @@
 require('chai').should();
-const {sortBySurname} = require('./index.js');
-const people = [
-  {
-    name: {
-      first: 'Blaise',
-      sur: 'Zabini',
-    },
+const {sortBySurname, getAgeDescriptions} = require('./index.js');
+
+const blaise = {
+  id: 0,
+  name: {
+    first: 'Blaise',
+    sur: 'Zabini',
   },
-  {
-    name: {
-      first: 'Hannah',
-      sur: 'Abbott',
-    },
+};
+const hannah = {
+  id: 1,
+  name: {
+    first: 'Hannah',
+    sur: 'Abbott',
   },
-  {
-    name: {
-      first: 'Ernie',
-      sur: 'Macmillan',
-    },
+};
+const ernie = {
+  id: 2,
+  name: {
+    first: 'Ernie',
+    sur: 'Macmillan',
   },
-];
+};
+const people = [blaise, hannah, ernie];
 
 describe('index.js', function() {
   describe('sortBySurname', function() {
     it('can sort a list of people by name', function() {
-      const expected = [
-        {name: {first: 'Hannah', sur: 'Abbott'}},
-        {name: {first: 'Ernie', sur: 'Macmillan'}},
-        {name: {first: 'Blaise', sur: 'Zabini'}},
-      ];
-      sortBySurname(people).should.deep.equal(expected);
+      // Act & Assert
+      sortBySurname(people).should.deep.equal([hannah, ernie, blaise]);
+    });
+  });
+
+  describe('getAgeDescriptions', function() {
+    it('can produce descriptions of people\'s ages.', function() {
+      // Arrange
+      const currentDate = new Date(2017, 3, 4);
+      const peopleDateOfBirth = {};
+      peopleDateOfBirth[blaise.id] = new Date(1979, 8, 2);
+      peopleDateOfBirth[ernie.id] = new Date(1980, 3, 21);
+
+      // Act
+      const getAgeDescriptionsForCurrentDate =
+        getAgeDescriptions(currentDate);
+      const getAgeDescriptionsWithDobs =
+        getAgeDescriptionsForCurrentDate(peopleDateOfBirth);
+      const ageDescriptions =
+        getAgeDescriptionsWithDobs(people);
+
+      // Assert
+      ageDescriptions.should.deep.equal([
+        'Blaise Zabini is 37 years old.',
+        'Hannah Abbott\'s age is unknown.',
+        'Ernie Macmillan is 36 years old.',
+      ]);
     });
   });
 });
