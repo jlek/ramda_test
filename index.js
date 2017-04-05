148348013388a1bde0f876c34686235039d50119
John <jlek@findmypast.com>
2017-04-05 08:47:24 +0100

    Started work on refactoring age description code.

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-const {curry, flip, map, path, pipe, prop, sortBy, toLower} = require('ramda');
+const {
+  __,
+  curry,
+  path,
+  pipe,
+  prop,
+  sortBy,
+  toLower,
+} = require('ramda');
 
 const getSurname = path(['name', 'sur']);
 const getSurnameLower = pipe(getSurname, toLower);
 const sortBySurname = sortBy(getSurnameLower);
 
-const getId = prop('id');
-const calculateYearDifference = (dateOne, dateTwo) => {
-  let yearDifference = dateOne.getFullYear() - dateTwo.getFullYear();
+const getDateOfBirth = (datesOfBirth) => pipe(
+  prop('id'),
+  prop(__, datesOfBirth)
+);
+const extraYearForMonth = (dateOne, dateTwo) => {
   const monthDifference = dateOne.getMonth() - dateTwo.getMonth();
-  if (
-    monthDifference < 0
-    || (monthDifference === 0 && dateOne.getDate() < dateTwo.getDate())
-  ) {
-    yearDifference--;
-  }
-  return yearDifference;
+  return monthDifference < 0
+    || (monthDifference === 0
+      && dateOne.getDate() < dateTwo.getDate());
 };
-// const calculateAge = (date, dateOfBirth) => 38; // TODO
+const calculateAge = (dateOne, dateTwo) =>
+  dateOne.getFullYear()
+    - dateTwo.getFullYear()
+    - (extraYearForMonth(dateOne, dateTwo) ? 1 : 0);
 const formatName = ({name}) => `${name.first} ${name.sur}`;
 const getAgeDescription = curry((date, datesOfBirth, person) => {
-  const dateOfBirth = pipe(
-    getId,
-    flip(prop)(datesOfBirth)
-  )(person);
-
+  const dateOfBirth = getDateOfBirth(datesOfBirth)(person);
   return dateOfBirth
-    ? `${formatName(person)} is ${calculateYearDifference(date, dateOfBirth)} years old.`
+    ? `${formatName(person)} is ${calculateAge(date, dateOfBirth)} years old.`
     : `${formatName(person)}'s age is unknown.`;
 });
-const getAgeDescriptions = curry((date, datesOfBirth, people) => {
-  return map(getAgeDescription(date, datesOfBirth), people);
-});
+const getAgeDescriptions = curry((date, datesOfBirth, people) =>
+  people.map(getAgeDescription(date, datesOfBirth)));
 
 module.exports = {
   sortBySurname,
